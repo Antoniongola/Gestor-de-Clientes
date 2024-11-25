9332868d942124daed15258168f3e891ea2a2d9f
antoniongola <antoniongola10@gmail.com>
2024-11-25 13:51:23 +0100

    fix (repository): updating the returning list of users method to page<User>.

--- src/main/java/com/ngolajr/cadastrodeclientes/repository/UserRepository.java
+++ src/main/java/com/ngolajr/cadastrodeclientes/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.ngolajr.cadastrodeclientes.repository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import com.ngolajr.cadastrodeclientes.model.Role;
 import com.ngolajr.cadastrodeclientes.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +12,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAllByRolesContaining(Role role);
     Optional<User> findByUsername(String username);
+    Page<User> findAll(Pageable pageable);
 
 }
