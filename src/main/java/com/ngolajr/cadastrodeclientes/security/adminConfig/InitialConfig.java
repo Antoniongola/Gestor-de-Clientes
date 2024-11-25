505491c4239018dbb54e0d732a07c898de6bb67f
antoniongola <antoniongola10@gmail.com>
2024-11-25 15:31:56 +0100

    fix (initial admin account): fixing the initial admin account auto generated, had forgotten to encrypt the password.

--- src/main/java/com/ngolajr/cadastrodeclientes/security/adminConfig/InitialConfig.java
+++ src/main/java/com/ngolajr/cadastrodeclientes/security/adminConfig/InitialConfig.java
@@ -5,6 +5,7 @@ import com.ngolajr.cadastrodeclientes.model.User;
 import com.ngolajr.cadastrodeclientes.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
@@ -13,12 +14,13 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class InitialConfig implements CommandLineRunner {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) throws Exception {
         User user = new User();
         user.setUsername("admin");
-        user.setPassword("admin");
+        user.setPassword(passwordEncoder.encode("admin"));
         user.setRoles(Set.of(Role.ADMIN, Role.USER, Role.MANAGER));
         if(userRepository.findByUsername("admin").isEmpty())
             userRepository.save(user);
