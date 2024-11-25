35eac3740e6324be823fbd38a8e6cb891d710393
antoniongola <antoniongola10@gmail.com>
2024-11-25 10:52:00 +0100

    feat (user): adding a new function to the user class (a function for converting a dto to user object)

--- src/main/java/com/ngolajr/cadastrodeclientes/model/User.java
+++ src/main/java/com/ngolajr/cadastrodeclientes/model/User.java
@@ -1,5 +1,6 @@
 package com.ngolajr.cadastrodeclientes.model;
 
+import com.ngolajr.cadastrodeclientes.model.dto.UserDto;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -12,9 +13,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String nome;
+    private String name;
     private String username;
     private String password;
     @OneToMany
     private Set<Role> roles;
+
+    public User userDtoToUser(UserDto userDto) {
+        User user = new User();
+        user.setName(userDto.name());
+        user.setUsername(userDto.username());
+        user.setPassword(userDto.password());
+        Set<Role> roles = Set.of(Role.USER);
+
+        return user;
+    }
 }
